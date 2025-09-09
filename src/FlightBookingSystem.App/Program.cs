079b2912256d5c95e259ff5f3c64fb55f4104012
Faris Adam Gunawan <FarisAdam.Gunawan@mitrais.com>
2025-09-09 14:09:29 +0700

    add dbcontext and checkconnection

--- src/FlightBookingSystem.App/Program.cs
+++ src/FlightBookingSystem.App/Program.cs
@@ -1,4 +1,6 @@
 ﻿using FlightBookingSystem.App.Menu;
+using FlightBookingSystem.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlightBookingSystem.App
 {
@@ -6,7 +8,51 @@ namespace FlightBookingSystem.App
     {
         static void Main(string[] args)
         {
-            LoginMenu.ShowLogin();
+            Console.Title = "Flight Booking System";
+
+            try
+            {
+                checkDbConnection();
+
+                LoginMenu.ShowLogin();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
         }
+
+        private static void checkDbConnection()
+        {
+            try
+            {
+                var dbBuilder = new DbContextOptionsBuilder<AppDbContext>();
+                dbBuilder.UseSqlite("Data Source=flightbooking.db");
+
+
+                using var dbContext = new AppDbContext(dbBuilder.Options);
+
+
+                Console.WriteLine("Initializing database...");
+                dbContext.Database.EnsureCreated();
+
+                var canConnect = dbContext.Database.CanConnect();
+
+                if (canConnect)
+                {
+                    Console.WriteLine("Database initialized successfully.");
+                }
+                else
+                {
+                    throw new Exception("Unable to connect to the database.");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
     }
 }
